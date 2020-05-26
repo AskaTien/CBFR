9f50bee4388ed766f2af84488d758259b27c04ae
AskaTien <constantine.sky@hotmail.com>
2020-05-26 18:18:50 +0800

    [Add] bufferviewer

---
+++ Assets/CBFR/Runtime/RendererFeatures/ClusterRenderPassFeature.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ClusterRenderPassFeature : ScriptableRendererFeature
+{
+    [System.Serializable]
+    public class ViewSettings
+    {
+        public TexBuffer texBuf = TexBuffer.Depth;
+    }
+
+    public enum TexBuffer
+    {
+        Depth,
+        WorldPosition,
+        Opaque
+    }
+
+    public ViewSettings settings = new ViewSettings();
+
+    class CustomRenderPass : ScriptableRenderPass
+    {
+        private Material blitMaterial = null;
+        public int blitShaderPassIndex = 0;
+        private RenderTargetIdentifier source { get; set; }
+        private RenderTargetHandle destination { get; set; }
+
+        RenderTargetHandle m_TemporaryColorTexture;
+        string m_ProfilerTag;
+
+        /// <summary>
+        /// Create the CopyColorPass
+        /// </summary>
+        public CustomRenderPass(Material material, string tag)
+        {
+            blitMaterial = material;
+            m_ProfilerTag = tag;
+            m_TemporaryColorTexture.Init("_TemporaryColorTexture");
+        }
+
+        /// <summary>
+        /// Configure the pass with the source and destination to execute on.
+        /// </summary>
+        /// <param name="source">Source Render Target</param>
+        /// <param name="destination">Destination Render Target</param>
+        public void Setup(RenderTargetIdentifier source, RenderTargetHandle destination)
+        {
+            this.source = source;
+            this.destination = destination;
+        }
+        // This method is called before executing the render pass.
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in an performance manner.
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerTag);
+
+            RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+            opaqueDesc.depthBufferBits = 0;
+
+            cmd.GetTemporaryRT(m_TemporaryColorTexture.id, opaqueDesc, FilterMode.Bilinear);
+            Blit(cmd, source, m_TemporaryColorTexture.Identifier(), blitMaterial, blitShaderPassIndex);
+            Blit(cmd, m_TemporaryColorTexture.Identifier(), source);
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        /// Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(m_TemporaryColorTexture.id);
+        }
+    }
+
+    CustomRenderPass m_ScriptablePass;
+
+    public override void Create()
+    {
+        m_ScriptablePass = new CustomRenderPass(CoreUtils.CreateEngineMaterial("Hidden/TextureView"), name);
+
+        // Configures where the render pass should be injected.
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+
+        m_ScriptablePass.blitShaderPassIndex = (int)settings.texBuf;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_ScriptablePass.Setup(renderer.cameraColorTarget, RenderTargetHandle.CameraTarget);
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
+
+
---
+++ Assets/CBFR/Shaders/TextureView.shader
@@ -0,0 +1,76 @@
+﻿Shader "Hidden/TextureView"
+{
+    SubShader
+    {
+        Tags { "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        HLSLINCLUDE
+
+
+
+        ENDHLSL
+
+        Pass
+        {
+            Name "Depth Texture"
+            HLSLPROGRAM
+                #pragma vertex Vert
+                #pragma fragment Frag
+            
+                #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+                #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
+                #include "Packages/com.unity.render-pipelines.universal/Shaders/PostProcessing/Common.hlsl"
+
+                half4 Frag(Varyings input) : SV_Target
+                {
+                    half4 col;
+                    col = SampleSceneDepth(input.uv);
+                    return col;
+                }
+            ENDHLSL
+        }
+
+        Pass
+        {
+            Name "World Position"
+            HLSLPROGRAM
+                #pragma vertex Vert
+                #pragma fragment Frag
+
+                #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+                #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
+                #include "Packages/com.unity.render-pipelines.universal/Shaders/PostProcessing/Common.hlsl"
+
+                half4 Frag(Varyings input) : SV_Target
+                {
+                    half4 col;
+                    col = SampleSceneDepth(input.uv);
+                    return col;
+                }
+            ENDHLSL
+        }
+
+        Pass
+        {
+            Name "Opaque Texture"
+            HLSLPROGRAM
+                #pragma vertex Vert
+                #pragma fragment Frag
+
+                #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+                #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareOpaqueTexture.hlsl"
+                #include "Packages/com.unity.render-pipelines.universal/Shaders/PostProcessing/Common.hlsl"
+
+                half4 Frag(Varyings input) : SV_Target
+                {
+                    half4 col;
+                    col.rgb = SampleSceneColor(input.uv);
+                    return col;
+                }
+            ENDHLSL
+        }
+    }
+}
